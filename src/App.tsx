d4f957804f670867342882c0a8b0f0e651e9e21b
Delmar Filho <ddlmar13@gmail.com>
2024-06-28 20:40:23 -0300

    create repository

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <div className="h-svh w-svw bg-slate-800">
+      <Button>AAAA</Button>
+    </div>
+  );
+}
+
+export default App;
